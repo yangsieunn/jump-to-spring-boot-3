b465c4e069dda619bba578473e5ddc07912a90db
yangsieun <didtldms2525@naver.com>
2024-08-08 00:28:06 +0900

    chapter 2-16

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,36 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+	
+	private final QuestionService questionService;
+	private final AnswerService answerService;
+	
+	@PostMapping("/create/{id}")  // URL 요청 시 createAnswer 호출 (PostMapping, POST 요청 처리)
+	public String createAnswer(Model model, @PathVariable("id") Integer id,
+	@Valid AnswerForm answerForm, BindingResult bindingResult) {
+		Question question = this.questionService.getQuestion(id);
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		this.answerService.create(question, answerForm.getContent());
+		return String.format("redirect:/question/detail/%s", id);  // 질문 조회
+	}
+}
